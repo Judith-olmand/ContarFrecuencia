22ac6a8840a0ccb7199e2095c7f6bae130ae5bac
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-02-06 19:50:50 +0100

    acabado.

--- src/ContarFrecuencia.java
+++ src/ContarFrecuencia.java
@@ -1,13 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class ContarFrecuencia {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Map<String, Integer> palabras = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("ContarPalabras.txt"))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                /**
+                 * Convierto todo en minusculas y "corto"
+                 * por un espacio, una coma o un punto.
+                 */
+                String[] palabra = linea.toLowerCase().split("[ ,.]+");
+
+                for (int i = 0; i < palabra.length; i++) {
+                    if (palabras.containsKey(palabra[i])) { // si la palabra está en el map
+                        palabras.put(palabra[i],palabras.get(palabra[i])+1); // sumo 1 a su valor
+                    } else {
+                        palabras.putIfAbsent(palabra[i],1); //si no está la añado y la doy de valor 1
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error al leer el archivo: " +
+                    e.getMessage());
+        }
+
+
+        for (Map.Entry<String, Integer> entry : palabras.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println("La palaba " + "\033[4m" + entry.getKey() + "\033[0m" + " aparece " + entry.getValue() + " veces.");
+            } else {
+                System.out.println("La palaba " + "\033[4m" + entry.getKey() + "\033[0m"  + " aparece " + entry.getValue() + " vez.");
+            }
+            //"\033[4m subraya; 3m cursiva; 1m negrita
+        }
+    }
 }
